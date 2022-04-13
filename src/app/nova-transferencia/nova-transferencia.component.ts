0383f87c5446c88a19bbc4b144dfeee68bfc442f
Wilian Souza <wilian.souza@dtidigital.com.br>
2022-04-13 13:31:00 -0300

    Exportando e importando variáveis.

--- src/app/nova-transferencia/nova-transferencia.component.ts
+++ src/app/nova-transferencia/nova-transferencia.component.ts
@@ -1,10 +1,26 @@
-import { Component } from "@angular/core";
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
-    selector: 'app-nova-transferencia',
-    templateUrl: './nova-transferencia.component.html',
-    styleUrls: ['./nova-transferencia.component.scss']
+  selector: 'app-nova-transferencia',
+  templateUrl: './nova-transferencia.component.html',
+  styleUrls: ['./nova-transferencia.component.scss'],
 })
-export class NovaTransferenciaComponent{
+export class NovaTransferenciaComponent {
 
+  valor: number;
+  destino: number;
+
+  @Output() aoTransmitir =  new EventEmitter<any>();
+
+  tranferir() {
+    console.log('Solicitado nova tranferencia');
+    const valorEmitir = { valor: this.valor, destino: this.destino }
+    this.aoTransmitir.emit(valorEmitir);
+    this.limparCampos();
+  }
+
+  limparCampos() {
+    this.valor = 0;
+    this.destino = 0;
+  }
 }
