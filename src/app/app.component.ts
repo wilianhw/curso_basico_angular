d94ef61db580b128b5d7e21eed2f0f8ea36556ba
Wilian Souza <wilian.souza@dtidigital.com.br>
2022-04-13 14:55:27 -0300

    Implementado service.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,17 @@
+import { TransferenciaService } from './services/transferencia.service';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'bytebank';
-  transferencias: any[] = [];
+
+  constructor(private service: TransferenciaService) {}
 
   transferir($event) {
-    console.log($event);
-    const transferencia = {...$event, data: new Date()}
-    this.transferencias.push(transferencia);
+    this.service.adicionar($event);
   }
 }
